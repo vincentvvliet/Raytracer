09dc02f8b2cc2a6493c5451834800681f9f675ca
DESKTOP-SUN5PO2\oskar <o.s.e.hage@student.tudelft.nl>
2022-10-19 15:37:00 +0200

    Testing to see how git works in visual studio

--- src/shading.h
+++ src/shading.h
@@ -3,7 +3,6 @@
 #include <framework/ray.h>
 
 // Compute the shading at the intersection point using the Phong model.
-const glm::vec3 computeShading (const glm::vec3& lightPosition, const glm::vec3& lightColor, const Features& features, Ray ray, HitInfo hitInfo);
-
+const glm::vec3 computeShading (const glm::vec3& lightPosition, const glm::vec3& lightColor, const Features& features, Ray ray, HitInfo hitInfo)
 // Given a ray and a normal (in hitInfo), compute the reflected ray in the specular direction (mirror direction).
 const Ray computeReflectionRay (Ray ray, HitInfo hitInfo);
