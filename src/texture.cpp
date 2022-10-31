a132ac65f7e07e01644bc842ff070ded0be15458
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2022-10-31 11:35:20 +0100

    Implement aquireTexel method

--- src/texture.cpp
+++ src/texture.cpp
@@ -3,10 +3,11 @@
 
 glm::vec3 acquireTexel(const Image& image, const glm::vec2& texCoord, const Features& features)
 {
-    // TODO: implement this function.
     // Given texcoords, return the corresponding pixel of the image
     // The pixel are stored in a 1D array of row major order
     // you can convert from position (i,j) to an index using the method seen in the lecture
     // Note, the center of the first pixel is at image coordinates (0.5, 0.5)
-    return image.pixels[0];
+
+    int index = 3 * (texCoord[1] * image.width + texCoord[0]);
+    return image.pixels[index];
 }
