c9404c5f06ed553354f02252de28c2f3db2ae116
DESKTOP-SUN5PO2\oskar <o.s.e.hage@student.tudelft.nl>
2022-11-02 20:42:52 +0100

    change segment scale to factor based on angle between lights and view

--- src/light.cpp
+++ src/light.cpp
@@ -16,8 +16,8 @@ int parallelogramLightPoints = 100;
 // you should fill in the vectors position and color with the sampled position and color
 std::list<PointLight> interpolateLine(const SegmentLight& light, std::list<PointLight> points, int segmentLightPoints) {
 
-    float scale = 1 / sqrt(segmentLightPoints);
-    for (int i = 0; i < sqrt(segmentLightPoints); i++) {
+    float scale = 1 / (100 * ((glm::dot(light.endpoint0, light.endpoint1) + 1) / 2));
+    for (int i = 0; i < 100*((glm::dot(light.endpoint0,light.endpoint1)+1)/2); i++) {
         glm::vec3 position = (light.endpoint0 - light.endpoint1) * (scale * i) + light.endpoint1;
         float alpha = (float)glm::distance(light.endpoint0, position) / glm::distance(light.endpoint0,light.endpoint1);
         glm::vec3 color = (1 - alpha) * light.color0 + alpha * light.color1;
@@ -169,7 +169,7 @@ glm::vec3 computeLightContribution(const Scene& scene, const BvhInterface& bvh, 
                    
                 }
 
-                total += color / glm::vec3 { sqrt(50), sqrt(50), sqrt(50) };
+                total += color / glm::vec3 { linepoints.size(), linepoints.size(), linepoints.size() };
             } else if (std::holds_alternative<ParallelogramLight>(light)) {
                 // TODO: add check for enableSoftShadows -> what to return when softShadows is disabled?
                 const ParallelogramLight parallelogramLight = std::get<ParallelogramLight>(light);
